feaa1047b53ab6ff841ce80bb0663b546c647205
shahar <shaharp2@gmail.com>
2020-06-21 16:55:56 +0300

    Updates

---
+++ precache-manifest.13dd0766073c007295559bd242d4ed84.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5d7e7d0db1bb11eb14ab74621ad667",
+    "url": "/layoutwork/index.html"
+  },
+  {
+    "revision": "898b9a2e8706784aa883",
+    "url": "/layoutwork/static/css/main.ff568d7d.chunk.css"
+  },
+  {
+    "revision": "b0a13a04b347cdd0b45b",
+    "url": "/layoutwork/static/js/2.0571d26f.chunk.js"
+  },
+  {
+    "revision": "138a6d51240ab070e8d1b20c54be6394",
+    "url": "/layoutwork/static/js/2.0571d26f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "898b9a2e8706784aa883",
+    "url": "/layoutwork/static/js/main.b9f2b960.chunk.js"
+  },
+  {
+    "revision": "df3ea4c8a60d2c13c374",
+    "url": "/layoutwork/static/js/runtime-main.7728d27a.js"
+  },
+  {
+    "revision": "376a8b95194fb33e0a203bf52f8cb467",
+    "url": "/layoutwork/static/media/bg-curvy-desktop.376a8b95.svg"
+  },
+  {
+    "revision": "5f4b8b3e0d56ac7578b1468584142faf",
+    "url": "/layoutwork/static/media/icon-access-anywhere.5f4b8b3e.svg"
+  },
+  {
+    "revision": "aedb3ae079501c43082669d8d427bf6b",
+    "url": "/layoutwork/static/media/icon-any-file.aedb3ae0.svg"
+  },
+  {
+    "revision": "794c5a484aefc6409c3a4e7aceb10eee",
+    "url": "/layoutwork/static/media/icon-arrow.794c5a48.svg"
+  },
+  {
+    "revision": "ca8c71cfbd876e461e850fcdcf9c9b29",
+    "url": "/layoutwork/static/media/icon-collaboration.ca8c71cf.svg"
+  },
+  {
+    "revision": "c90d96c2492a2d6f0863256f2bf9015d",
+    "url": "/layoutwork/static/media/icon-email.c90d96c2.svg"
+  },
+  {
+    "revision": "0f65814bd9f282828254e3382e223a57",
+    "url": "/layoutwork/static/media/icon-location.0f65814b.svg"
+  },
+  {
+    "revision": "47e54a44b879380ff8adb31f40249296",
+    "url": "/layoutwork/static/media/icon-phone.47e54a44.svg"
+  },
+  {
+    "revision": "f558537850876022cbf1d99e525724f4",
+    "url": "/layoutwork/static/media/icon-security.f5585378.svg"
+  },
+  {
+    "revision": "526544ae64c3d7a9cf06bc566728d2b9",
+    "url": "/layoutwork/static/media/illustration-intro.526544ae.png"
+  },
+  {
+    "revision": "99294b1f2757d664921ec643a982959a",
+    "url": "/layoutwork/static/media/illustration-stay-productive.99294b1f.png"
+  },
+  {
+    "revision": "c71bf7bf6985f3ac03fa893fb45bed38",
+    "url": "/layoutwork/static/media/logo.c71bf7bf.svg"
+  }
+]);
